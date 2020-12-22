635477b4ed2aebd94a282adde99588d9799d36d0
Valeriia Okolovych <okolovych.valeriia@gmail.com>
2020-12-22 14:45:18 +0200

    create file HW1_T2

---
+++ HW1/HW1_T2.py
@@ -0,0 +1,7 @@
+first_number = float(input("Input first number: "))
+second_number = float(input("Input second number: "))
+print("The result of the addition:", first_number + second_number, 
+    "\nThe result of the subtraction:", first_number - second_number,
+    "\nThe result of the multiplication:", first_number * second_number,
+    "\nThe result of the division:", first_number / second_number,
+    "\nThe result of the exponentiation:", first_number ** second_number)
